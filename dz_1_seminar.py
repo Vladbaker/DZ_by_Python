7920afec8e48575cc1892ccb535ab632770d887c
Vladislav Tkachenko <Vladislav_tkachenko_93@mail.ru>
2022-10-12 21:27:18 +0300

    Save 3 dz python

--- dz_1_seminar.py
+++ dz_1_seminar.py
@@ -11,16 +11,30 @@
 # Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
 #  в которой находится эта точка (или на какой оси она находится).
 
-X = int(input('Введите координату X '))
-Y = int(input('Введите координату Y '))
+# X = int(input('Введите координату X '))
+# Y = int(input('Введите координату Y '))
 
-if X == 0 and Y == 0:
+# if X == 0 and Y == 0:
+#     print('Ошибка')
+# if X > 0 and Y > 0:
+#     print('1')
+# if X > 0 and Y < 0:
+#     print('4')
+# if X < 0 and Y < 0:
+#     print('3')
+# if X < 0 and Y > 0:
+#     print('2')
+
+# Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+quarter_number = int(input('Введите номер четверти: '))
+if quarter_number < 1 or quarter_number > 4:
     print('Ошибка')
-if X > 0 and Y > 0:
-    print('1')
-if X > 0 and Y < 0:
-    print('4')
-if X < 0 and Y < 0:
-    print('3')
-if X < 0 and Y > 0:
-    print('2')
+elif quarter_number == 1:
+    print('x > 0 и y > 0')
+elif quarter_number == 2:
+    print('x < 0 и y > 0')
+elif quarter_number == 3:
+    print('x < 0 и y < 0')
+elif quarter_number == 4:
+    print('x > 0 и y < 0')
